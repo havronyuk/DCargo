6e2420ab20acefed38ecd307d4c04ab9d919d74d
Havronyuk <alexey.havronyuk@gmail.com>
2026-07-16 15:36:10 +0300

    + драйвер кард

--- src/main/java/dccargo/dcargoservice/controller/DriverCardController.java
+++ src/main/java/dccargo/dcargoservice/controller/DriverCardController.java
@@ -1,2 +1,56 @@
-package dccargo.dcargoservice.controller;public class DriverCardController {
+package dccargo.dcargoservice.controller;
+
+
+import dccargo.dcargoservice.model.dcargo.DriverCard;
+import dccargo.dcargoservice.model.dcargo.Passport;
+import dccargo.dcargoservice.service.dcargo.DriverCardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor  // генерирует конструктор для всех final полей
+public class DriverCardController {
+
+    private final DriverCardService driverCardService;
+
+    @PostMapping("/createDriverCard")
+    public ResponseEntity<DriverCard> create(@RequestBody DriverCard driverCard) {
+        DriverCard savedCard = driverCardService.create(driverCard);
+
+        return ResponseEntity.ok(savedCard);
+    }
+
+    @PostMapping("/updateDriverCard")
+    public ResponseEntity<DriverCard> update(@RequestBody DriverCard driverCard) {
+
+        DriverCard savedCard = driverCardService.update(driverCard);
+
+        return ResponseEntity.ok(savedCard);
+    }
+
+    @PostMapping("/deactivateDriverCard")
+    public ResponseEntity<?> deactivatePassport(@RequestParam("idDriverCard") Integer idDriverCard){
+        Map<String,Object> response = new HashMap<>();
+        try {
+
+            response = driverCardService.deactivateDriverCard(idDriverCard);
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+
+
 }
--- src/main/java/dccargo/dcargoservice/model/dcargo/DriverCard.java
+++ src/main/java/dccargo/dcargoservice/model/dcargo/DriverCard.java
@@ -1,2 +1,39 @@
-package dccargo.dcargoservice.model.dcargo;public class DriverCard {
+package dccargo.dcargoservice.model.dcargo;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "driver_card")
+public class DriverCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_driver_card")
+    private Integer idDriverCard;
+
+    @Column(name = "number", length = 255)
+    private String number;
+
+    @Column(name = "issue_date")
+    private LocalDate issueDate;
+
+    @Column(name = "expiry_date")
+    private LocalDate expiryDate;
+
+    @Column(name = "issued_by", columnDefinition = "TEXT")
+    private String issuedBy;
+
+    @Column(name = "categories", length = 255)
+    private String categories;
+
+    @Column(name = "block", nullable = false)
+    private Boolean block = false;
+
+
 }
