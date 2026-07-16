6fa73d0a44aca3f3d309030c8130f962fc3f168b
RedMercury-14 <98361735+RedMercury-14@users.noreply.github.com>
2026-07-16 09:36:49 +0300

    добавление модели и репозитория для файлов

---
+++ src/main/java/dccargo/dcargoservice/repository/dcargo/FileCustomRepository.java
@@ -0,0 +1,9 @@
+package dccargo.dcargoservice.repository.dcargo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dccargo.dcargoservice.model.dcargo.FIleCustom;
+
+public interface FileCustomRepository extends JpaRepository<FIleCustom, Long> {
+
+}
