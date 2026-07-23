843d72f7f426663122ff9c04621125cece5420fc
Havronyuk <alexey.havronyuk@gmail.com>
2026-07-23 14:36:54 +0300

    работа с внешним апи ордера

--- src/main/java/dccargo/dcargoservice/controller/InternalController.java
+++ src/main/java/dccargo/dcargoservice/controller/InternalController.java
@@ -1,8 +1,13 @@
 package dccargo.dcargoservice.controller;
 
+import dccargo.dcargoservice.model.dcargo.Order;
+import dccargo.dcargoservice.model.dcargo.Passport;
 import dccargo.dcargoservice.service.dcargo.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +19,12 @@ public class InternalController {
 
     private final OrderService orderService;
 
+    @PostMapping("/createOrderFromShipment")
+    public ResponseEntity<Order> createOrderFromShipment(@RequestBody Order order) {
+        Order savedOrder = orderService.createOrderFromShipment(order);
 
-
+        return ResponseEntity.ok(savedOrder);
+    }
 
 
 }
